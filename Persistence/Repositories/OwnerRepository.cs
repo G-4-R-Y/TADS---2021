e43164c3e7708a14eeac0721713249ea9cbede27
G-4-R-Y <rcf.jr@hotmail.com>
2021-03-19 22:36:15 -0300

    19-03-21

---
+++ Persistence/Repositories/OwnerRepository.cs
@@ -0,0 +1,40 @@
+﻿using PetCare.Domain.Models;
+using PetCare.Domain.Repositories;
+using PetCare.Persistence.Data;
+using System;
+
+namespace PetCare.Persistence.Repositories
+{
+    public class OwnerRepository : IOwnerRepository
+    {
+        private PetCareDataContext _context = new PetCareDataContext();
+        public OwnerModel GetByID(Guid ownerID)
+        {
+            throw new NotImplementedException();
+        }
+        public OwnerModel GetByEMail(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Create(OwnerModel ownerModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(OwnerModel ownerModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(OwnerModel ownerModel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
